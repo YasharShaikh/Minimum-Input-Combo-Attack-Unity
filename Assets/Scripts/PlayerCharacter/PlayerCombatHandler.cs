e01699236db18f96068be4f119bce46979d9583d
Yashar Shaikh <yashrsk6@gmail.com>
2024-03-27 16:49:53 +0530

    combat system

--- Assets/Scripts/PlayerCharacter/PlayerCombatHandler.cs
+++ Assets/Scripts/PlayerCharacter/PlayerCombatHandler.cs
@@ -3,44 +3,118 @@ using UnityEngine;
 
 public class PlayerCombatHandler : MonoBehaviour
 {
-
     [SerializeField] float timeBetweenCombo;
     [SerializeField] int mainAtkComboStream; // will track the current combo count
-    [SerializeField] string[] swdATKTracker = { "sword attack 1", "sword attack 2", "sword attack 3" }; // will track which swd atk animation is to be played
-    [SerializeField] string[] pwrATKTracker = { "power attack 1", "power attack 2", "power attack 3" }; // will track which pwr atk animation is to be played
+    [SerializeField] string[] swdATKTracker ; // will track which swd atk animation is to be played
+    [SerializeField] string[] pwrATKTracker ; // will track which pwr atk animation is to be played
     [SerializeField] string[] performedComboTracker;
     int maxCombo = 3;
-    float lastClickTime;
+    [SerializeField] float lastClickTime;
     bool isPerformingCombo;
     bool isPerformingATK;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        mainAtkComboStream = 0;
+        lastClickTime = 0f;
+        isPerformingCombo = false;
+        isPerformingATK = false;
     }
 
     // Update is called once per frame
     void Update()
     {
 
-        //check if button pressed swd or pwr
-        //check if this is a fresh combo attack then set isPerformingCombo=true
-        //if current mainATKcombostream is 1 and button pressed is swd then perform 1st swd || current mainATKcombostream is 1 and button pressed is pwr then perform 1st pwr
-        //similarly for mainATKcombostream 2 and mainATKcombostream 3 
-
-
+        // Check if the player has triggered a sword or power attack
         if (InputHandler.instance.swordATKTriggered || InputHandler.instance.powerATKTriggered)
         {
-            if(mainAtkComboStream == 0)
+            // If the current main attack combo stream is zero, start a new combo
+            if (mainAtkComboStream == 0)
             {
-
+                StartCombo();
             }
-            mainAtkComboStream++;
-            Debug.Log("Current combo count= " + mainAtkComboStream);
-        }
+            else if (mainAtkComboStream < maxCombo)
+            {
+                // Check if the time between combo attacks has elapsed
+                if (Time.time - lastClickTime < timeBetweenCombo)
+                {
+                    // Continue the combo attack
+                    ContinueCombo();
+                }
+                else
+                {
+                    // Start a new combo attack
+                    StartCombo();
+                }
+            }
+            else
+            {
+                // Maximum combo reached, reset combo stream
+                ResetCombo();
+            }
 
+            // Update the last click time
+            lastClickTime = Time.time;
+        }
     }
 
+    // Start a new combo attack
+    void StartCombo()
+    {
+        mainAtkComboStream++;
+        Debug.Log("Starting combo " + mainAtkComboStream);
 
+        // Perform the appropriate attack based on the current combo stream
+        if (InputHandler.instance.swordATKTriggered)
+        {
+            PerformSwordAttack();
+        }
+        else if (InputHandler.instance.powerATKTriggered)
+        {
+            PerformPowerAttack();
+        }
+    }
+
+    // Continue the current combo attack
+    void ContinueCombo()
+    {
+        Debug.Log("Continuing combo " + mainAtkComboStream);
+
+        // Perform the appropriate attack based on the current combo stream
+        if (InputHandler.instance.swordATKTriggered)
+        {
+            PerformSwordAttack();
+        }
+        else if (InputHandler.instance.powerATKTriggered)
+        {
+            PerformPowerAttack();
+        }
+    }
+
+    // Reset the combo attack
+    void ResetCombo()
+    {
+        Debug.Log("Combo reset");
+        mainAtkComboStream = 0;
+    }
+
+    // Perform a sword attack
+    void PerformSwordAttack()
+    {
+        if (swdATKTracker.Length == 0)
+        {
+            Debug.Log("Empty");
+        }
+
+        Debug.Log("Performing sword attack: " + swdATKTracker[mainAtkComboStream - 1]);
+        // Play the corresponding animation for the sword attack
+    }
+
+    // Perform a power attack
+    void PerformPowerAttack()
+    {
+        Debug.Log("Performing power attack: " + pwrATKTracker[mainAtkComboStream - 1]);
+        // Play the corresponding animation for the power attack
+    }
 }
